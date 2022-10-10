e689eb0cbe9ec454ae5d8eabb84593bd71a82dd6
Hakjun <hiatus9014@gmail.com>
2022-10-10 20:01:11 +0900

    code up 31~40

---
+++ src/Codeup/Main_1031.java
@@ -0,0 +1,15 @@
+package Codeup;
+
+import java.util.Scanner;
+
+public class Main_1031 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        String num8 = Integer.toOctalString(input);
+
+        System.out.println(num8);
+
+
+    }
+}
