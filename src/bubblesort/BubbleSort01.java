dd760b97dcda79a54cd0ea18c5513f6e7b04731b
Hakjun <hiatus9014@gmail.com>
2022-10-13 10:08:52 +0900

    bubblesort 이중for문

---
+++ src/bubblesort/BubbleSort01.java
@@ -0,0 +1,31 @@
+package bubblesort;
+
+import java.util.Arrays;
+
+public class BubbleSort01 {
+
+    public int[] sort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[0] < arr[j]) {
+                    System.out.println(arr[j]);
+                    if(arr[i]>arr[j]) {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+        }
+
+        return arr;
+    }
+    public static void main(String[] args) {
+        int[] arr = new int[]{7,2,3,9,28,11};
+        //오름차순 또는 내림차순으로 정렬하는 알고리즘을 짜보세요
+        BubbleSort01 bs = new BubbleSort01();
+        int[] result = bs.sort(arr);
+        System.out.println(Arrays.toString(result));
+
+    }
+}
